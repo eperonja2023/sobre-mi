17549b83d6561ed7a415cb87d6b9e41cacc5c297
eperonja2023 <eperonja@yahoo.com.ar>
2023-08-12 08:38:34 -0300

    subo modificaciones en html, css, js

--- practico1/calculadora.js
+++ practico1/calculadora.js
@@ -1,146 +1,38 @@
-// const readlineSync = require('readline-sync');
+function calcular() {
+    let num1 = parseInt(document.getElementById("valor1").value);
+    let num2 = parseInt(document.getElementById("valor2").value);
+    let operando = document.getElementById("operando").value;
 
+    // validación de los operandos
+    if (isNaN(num1) || isNaN(num2)) {
+        alert("solo se admiten valores numéricos");
+    }
 
-// function funcionesDisponibles() {
-//     let option = readlineSync.question(
-//         'Las operaciones disponibles son: \n' +
-//         '(1) Suma \n' +
-//         '(2) Resta \n' +
-//         '(3) Multiplicacion \n' +
-//         '(4) Division \n' +
-//         '(5) Promedio \n' +
-//         '(6) Desviacion Estandar \n' +
-//         '(7) Factorial \n' +
-//         'Por favor ingrese el numero de la operacion que desea realizar: '
-//     );
-//     return Number(option);
-// }
+    if (operando == "division" && num2 == 0) {
+        alert("NO SE PUEDE DIVIDIR POR CERO");
 
-function suma(numeros) {
-    // let resultado = 0;
-    // for (let i = 0; i < numeros.length; i++) {
-    //     resultado += numeros[i];
-    // }
-    // return resultado;
+    }
 
-    return numeros.reduce((resultado, e) => resultado + e, 0);
-}
-
-function resta(numeros) {
-    // let resultado = numeros.shift();
-    // for (let i = 0; i < numeros.length; i++) {
-    //     resultado -= numeros[i];
-    // }
-    // return resultado;
-
-    let valorInicial = numeros.shift();
-    return numeros.reduce((resultado, e) => resultado - e, valorInicial);
-}
-
-function multiplicacion(numeros) {
-    // let resultado = 1;
-    // for (let i = 0; i < numeros.length; i++) {
-    //     resultado *= numeros[i];
-    // }
-    // return resultado;
-
-    return numeros.reduce((resultado, e) => resultado * e, 1);
-}
-
-function division(numeros) {
-    // [2, 3, 4, 5] => ((2 / 3) / 4) / 5
-    // let resultado = numeros.shift();
-    // for (let i = 0; i < numeros.length; i++) {
-    //     resultado /= numeros[i];
-    // }
-    // return resultado;
-
-    let valorInicial = numeros.shift();
-    return numeros.reduce((resultado, e) => resultado / e, valorInicial);
-}
-
-// function promedio(numeros) {
-//     // let resultado = 0;
-//     // for (let i = 0; i < numeros.length; i++) {
-//     //     resultado += numeros[i];
-//     // }
-//     // resultado /= numeros.length;
-//     // return resultado;
-
-//     let sum = suma(numeros);
-//     return sum / numeros.length;
-// }
-
-// function desviacionEstandar(numeros) {
-//     let prom = promedio(numeros);
-//     let N = numeros.length;
-//     let sum = 0;
-
-//     for (let i = 0; i < N; i++) {
-//         sum += (numeros[i] - prom)**2;
-//     }
-//     sum /= N;
-//     return Math.sqrt(sum);
-// }
-
-// function factorial(numero) {
-//     let resultado = 1;
-//     for (let i = 2; i <= numero; i++) {
-//         resultado *= i;
-//     }
-//     return resultado;
-// }
-
-function calcularResultado(option, numeros) {
+    // cálculo de las operaciones + resultado en display
     let resultado;
-    switch (parseInt(option)) {
-        case 1:
-            resultado = suma(numeros);
+    switch (operando) {
+        case ("suma"):
+            resultado = num1 + num2;
             break;
-        case 2:
-            resultado = resta(numeros);
+        case ("resta"):
+            resultado = num1 - num2;
             break;
-        case 3:
-            resultado = multiplicacion(numeros);
+        case ("multiplicacion"):
+            resultado = num1 * num2;
             break;
-        case 4:
-            resultado = division(numeros);
-    //         break;
-    //     case 5:
-    //         resultado = promedio(numeros);
-    //         break;
-    //     case 6:
-    //         resultado = desviacionEstandar(numeros);
-    //         break;
-    //     case 7:
-    //         resultado = factorial(numeros);
-    // }
-    return resultado;
+        case ("division"):
+            resultado = num1 / num2;
+            break;
+        default:
+            alert("Se deben completar los 3 campos");
+    }
+
+    document.getElementById("resultado").innerHTML = resultado;
 }
 
-// function main() {
-//     let funcionElegida = funcionesDisponibles();
-//     while (![1, 2, 3, 4, 5, 6, 7].includes(funcionElegida)) {
-//         console.clear();
-//         console.log("La opcion ingresada es incorrecta, por favor ingrese nuevamente");
-//         funcionElegida = funcionesDisponibles();
-//     }
-
-//     if (funcionElegida === 7) {
-//         numero = readlineSync.questionFloat('Ingrese un numero: ');
-//         resultado = factorial(numero);
-//     } else {
-//         let numeroOperaciones = readlineSync.questionInt("Ingrese la cantidad de numeros a operar: ");
-//         let numeros = [];
-//         let numero;
-//         for (let i = 0; i < numeroOperaciones; i++) {
-//             numero = readlineSync.questionFloat('Ingrese un numero: ');
-//             numeros.push(numero);
-//         }
-//         resultado = calcularResultado(funcionElegida, numeros);
-//     }
-
-//     console.log(`El resultado de la operacion es: ${resultado}`);
-// }
-
-main();
+document.getElementById("boton").addEventListener("click", calcular);
