87d1d24b3a95b761ed2a14602a37791a2529fa64
eperonja2023 <eperonja@yahoo.com.ar>
2023-08-15 07:09:50 -0300

    Subo el README empezado + detalles CCS + JS terminado

---
+++ practico1/README.md
@@ -0,0 +1,19 @@
+# CALCULADORA
+Programa de fácil uso ejecutable desde navegadores de internet que consiste en una calculadora con operaciones basicas (suma-resta-multiplicacion-division) construído mediante HTTP, CSS y JS.
+___
+## Comenzando
+
+### Pre-requisitos
+Para acceder a los archivos del proyecto es recomendable tener instalado VS Code y Git Bash. 
+
+### Instalacion
+Para obtener una copia del proyecto en funcionamiento en la máquina local para propositos de desarrollo y pruebas:
+* Posicionarse en la carpeta donde desee descargar el proyecto
+* Abrir el programa Git Bash y ejecutar el siguiente comando para iniciar la descarga:
+ > git clone https://github.com/eperonja2023/sobre-mi.git
+
+### Para ejecutar el programa
+* Desde la máquina local abrir el archivo index.html descargado en la carpeta practico1
+* Ó desde Internet usando cualquier navegador desde este enlace:  [Calculadora](https://eperonja2023.github.io/sobre-mi/practico1)
+___
+## Ejecutando las pruebas
--- practico1/calculadora.js
+++ practico1/calculadora.js
@@ -1,20 +1,32 @@
 function calcular() {
-    let num1 = parseInt(document.getElementById("valor1").value);
-    let num2 = parseInt(document.getElementById("valor2").value);
+    console.log('Corriendo funcion "calcular"');
+
+    let num1 = document.getElementById("valor1").value;
+    let num2 = document.getElementById("valor2").value;
     let operando = document.getElementById("operando").value;
 
-    // validación de los operandos
-    if (isNaN(num1) || isNaN(num2)) {
-        alert("solo se admiten valores numéricos");
+// validación de los operandos
+    //  ^[+-]?[0-9]+([.][0-9]+)?$
+    // /^[+-]?[0-9]+([.][0-9]+)?$/
+
+    let exprRegulNumero = /^[+-]?[0-9]+([.][0-9]+)?$/;
+    let esNumero = exprRegulNumero.test(num1) && exprRegulNumero.test(num2);
+
+    if (!esNumero) {
+        alert("solo se pueden ingresar valores numericos");
         return;
     }
 
-    if (operando == "division" && num2 == 0) {
+    if (operando === "division" && num2 == 0) {
         alert("NO SE PUEDE DIVIDIR POR CERO");
         return;
     }
+    
+// luego de validar los operandos los convierto a numeros para poder calcular
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
 
-    // cálculo de las operaciones + resultado en display
+// cálculo de las operaciones + resultado en display
     let resultado;
     switch (operando) {
         case ("suma"):
@@ -37,3 +49,17 @@ function calcular() {
 }
 
 document.getElementById("boton").addEventListener("click", calcular);
+
+
+
+
+function borrar() {
+    document.getElementById("valor1").value = "";
+    document.getElementById("valor2").value = "";
+    document.getElementById("operando").value = "";
+    document.getElementById("resultado").innerHTML = "";
+    return;
+}
+
+// operando1.reset();
+// let operando1 = document.getElementById("laId")
